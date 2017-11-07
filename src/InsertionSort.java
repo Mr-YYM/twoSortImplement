d6af356dd7daefe4bd538ff1bf51f6ec1241f53a
Yeming <492851378@qq.com>
2017-11-07 21:53:27 +0800

    程序完善

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -2,7 +2,7 @@ public class InsertionSort {
     public static void sort(Comparable[] arr) {
         int n = arr.length;
         for (int i = 1; i < n; i++) {
-            for (int j = i; j > 0 && arr[j].compareTo(arr[j - 1]) < 0; j--) {
+            for (int j = i; j > 0; j--) {
                 if (arr[j].compareTo(arr[j - 1]) < 0) {
                     swap(arr, j, j - 1);
                 } else {
